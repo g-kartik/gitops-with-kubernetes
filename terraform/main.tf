26cc14dd598d67f061b501cc3dd74e0a171050ee
karthik <gkraja@mindkosh.com>
2024-05-16 11:07:02 +0000

    Add terraform config for google cloud
    Create google cloud service account

---
+++ terraform/main.tf
@@ -0,0 +1,54 @@
+terraform {
+    required_providers {
+        google = {
+            source = "hashicorp/google"
+            version = "5.29.1"
+        }
+        local = {
+            source = "hashicorp/local"
+            version = "2.5.1"
+        }
+        time = {
+            source = "hashicorp/time"
+            version = "0.11.1"
+        }
+    }
+}
+
+provider "google" {
+    project = "playground-gkarthik"
+}
+
+provider "local" {
+
+}
+
+locals {
+    service_account_private_key_path = abspath("${path.root}/${var.service_account_private_key_filename}")
+}
+
+resource "google_service_account" "service_account" {
+    account_id   = "playground-gitops"
+    display_name = "GitOps Service Account"
+}
+
+resource "time_rotating" "google_service_key_rotation" {
+    rotation_days = 30
+}
+
+resource "google_service_account_key" "service_account_key" {
+    service_account_id = google_service_account.service_account.id
+    private_key_type = "TYPE_GOOGLE_CREDENTIALS_FILE"
+    key_algorithm = "KEY_ALG_RSA_2048"
+
+    keepers = {
+        rotation_time = time_rotating.google_service_key_rotation.rotation_rfc3339
+    }
+}
+
+resource "local_sensitive_file" "service_account_private_key" {
+    filename = local.service_account_private_key_path
+    content = base64decode(google_service_account_key.service_account_key.private_key)
+}
+
+
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "service_account_private_key_filename" {
+    type = string
+    default = "gcloud_service_account_private_key.json"
+}
